673b4cfcb981b91b1a2234abc4fa1ea6c8158935
DAB0mB <emanor6@gmail.com>
2017-01-24 01:41:16 -0200

    Get rid of string literals

--- resources/cpp/src/utils.cpp
+++ resources/cpp/src/utils.cpp
@@ -4,7 +4,7 @@
 #include <emscripten/bind.h>
 #include "utils.h"
 
-namespace utils {
+namespace Utils {
   int mod(double context, double num) {
     return std::fmod((std::fmod(context, num) + num), num);
   }
@@ -12,9 +12,9 @@ namespace utils {
   double trim(double context, int decimals, const std::string mode) {
     double accumulator = context * std::pow(10, decimals);
 
-    if (mode.compare(literals::ceil) == 0)
+    if (mode.compare("ceil") == 0)
       accumulator = std::ceil(accumulator);
-    else if (mode.compare(literals::floor) == 0)
+    else if (mode.compare("floor") == 0)
       accumulator = std::floor(accumulator);
     else
       accumulator = std::round(accumulator);
@@ -23,45 +23,45 @@ namespace utils {
   }
 
   int isBetween(double context, double num1, double num2, const std::string precision) {
-    return compare(context, std::min(num1, num2), literals::gte, precision) &&
-           compare(context, std::max(num1, num2), literals::lte, precision);
+    return compare(context, std::min(num1, num2), ">=", precision) &&
+           compare(context, std::max(num1, num2), "<=", precision);
   }
 
   int compare(double context, double num, const std::string method, const std::string precision) {
     // Fixed precision, "almost equal" with a deviation of ε
-    if (precision.compare(literals::fixed)) {
-      if (method.compare(literals::lt) == 0 ||
-          method.compare(literals::lte) == 0)
+    if (precision.compare("fixed")) {
+      if (method.compare("<") == 0 ||
+          method.compare("<=") == 0)
         return context <= num + DBL_EPSILON;
-      if (method.compare(literals::gt) == 0 ||
-          method.compare(literals::gte) == 0)
+      if (method.compare(">") == 0 ||
+          method.compare(">=") == 0)
         return context >= num - DBL_EPSILON;
       return std::abs(context - num) <= DBL_EPSILON;
     }
     // Pixel precision, round comparison
-    else if (precision.compare(literals::pixel)) {
-      if (method.compare(literals::lt) == 0 ||
-          method.compare(literals::lte) == 0)
+    else if (precision.compare("pixel")) {
+      if (method.compare("<") == 0 ||
+          method.compare("<=") == 0)
         return std::round(context) <= std::round(num);
-      if (method.compare(literals::gt) == 0 ||
-          method.compare(literals::gte) == 0)
+      if (method.compare(">") == 0 ||
+          method.compare(">=") == 0)
         return std::round(context) >= std::round(num);
       return std::round(context) == std::round(num);
     }
     // Exact precision
     else {
-      if (method.compare(literals::lt) == 0) return context < num;
-      if (method.compare(literals::lte) == 0) return context <= num;
-      if (method.compare(literals::gt) == 0) return context > num;
-      if (method.compare(literals::gte) == 0) return context >= num;
+      if (method.compare("<") == 0) return context < num;
+      if (method.compare("<=") == 0) return context <= num;
+      if (method.compare(">") == 0) return context > num;
+      if (method.compare(">=") == 0) return context >= num;
       return context == num;
     }
   }
 }
 
 EMSCRIPTEN_BINDINGS(utils_module) {
-  emscripten::function("utils_mod", &utils::mod);
-  emscripten::function("utils_trim", &utils::trim);
-  emscripten::function("utils_isBetween", &utils::isBetween);
-  emscripten::function("utils_compare", &utils::compare);
+  emscripten::function("utils_mod", &Utils::mod);
+  emscripten::function("utils_trim", &Utils::trim);
+  emscripten::function("utils_isBetween", &Utils::isBetween);
+  emscripten::function("utils_compare", &Utils::compare);
 }
--- resources/cpp/src/utils.h
+++ resources/cpp/src/utils.h
@@ -1,17 +1,4 @@
-#include <string>
-
-namespace utils {
-  namespace literals {
-    const std::string ceil = "ceil";
-    const std::string floor = "floor";
-    const std::string gt = ">";
-    const std::string gte = ">=";
-    const std::string lt = "<";
-    const std::string lte = "<=";
-    const std::string fixed = "f";
-    const std::string pixel = "px";
-  }
-
+namespace Utils {
   int mod(double context, double num);
   double trim(double context, int decimals, const std::string mode);
   int isBetween(double context, double num1, double num2, const std::string precision);
